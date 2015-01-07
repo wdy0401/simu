b839e275f27ada40740e5e92f3cbd82a2a8c46aa
wdy0401 <wdy0401@gmail.com>
2015-01-07 17:09:32 +0800

    0 main

--- main.cpp
+++ main.cpp
@@ -1,10 +1,31 @@
 #include "mainwindow.h"
 #include <QApplication>
+
+#include"datafeed.h"
+#include"match_engine.h"
+#include"tactic.h"
+#include"snapshot.h"
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+
+    datafeed * df =new datafeed;
+    match_engine * me =new match_engine;
+    tactic * tc =new tactic;
+    snapshot * ss = new snapshot;
+
+    //首先把指针放入相应的其他类里面 以便内部调用
+    //然后设置好类型之间的信号槽关系
+
+    QObject::connect(df,&datafeed::send_quote,me,&match_engine::rec_quote);
+    QObject::connect(me,&match_engine::send_quote,tc,&tactic::quote);
+    QObject::connect(df,&datafeed::send_quote,me,&match_engine::rec_quote);
+    QObject::connect(df,&datafeed::send_quote,me,&match_engine::rec_quote);
+    QObject::connect(df,&datafeed::send_quote,me,&match_engine::rec_quote);
+    QObject::connect(df,&datafeed::send_quote,me,&match_engine::rec_quote);
 
     return a.exec();
 }
